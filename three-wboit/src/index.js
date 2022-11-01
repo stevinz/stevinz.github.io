2a5383c3337367ea16e5f8ee825d58c979c3379e
Stephens Nunnally <stevinz@gmail.com>
2022-10-31 19:47:14 -0700

    Local test

---
+++ three-wboit/src/index.js
@@ -0,0 +1,3 @@
+export { MeshWboitMaterial } from './materials/MeshWboitMaterial.js';
+export { WboitCompositeShader } from './shaders/WboitCompositeShader.js';
+export { WboitPass } from './WboitPass.js';
