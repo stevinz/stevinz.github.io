f95e7378bc40c1be55675cc70ffaaf5ed737297b
Stephens Nunnally <stevinz@gmail.com>
2022-11-02 14:04:24 -0700

    Improved iOS rendering

--- three-wboit/src/index.js
+++
@@ -1,3 +0,0 @@
-export { MeshWboitMaterial } from './materials/MeshWboitMaterial.js';
-export { WboitCompositeShader } from './shaders/WboitCompositeShader.js';
-export { WboitPass } from './WboitPass.js';
